334a33b717f8b0847d9a7894b85e281ad343e56c
mohdazeem0 <mohdazm007@gmail.com>
2021-08-26 16:41:58 +0530

    update

--- Problem1.js
+++ Problem1.js
@@ -1,3 +1,6 @@
 const arr = [null, 2, 3, 4, "cat", { num: 25 }, undefined, 2];
-const newArr = arr.filter((item) => typeof item === "numberr" && item);
-console.log(newArr.reduce(acc, (num) => acc + b));
+const result = arr
+  .filter((item) => typeof item === "numberr" && item)
+  .reduce((acc, num) => acc + num);
+
+console.log(result);
