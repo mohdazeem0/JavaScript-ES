9cad25c153f90520f78d35e9168613dd62843abb
mohdazeem0 <mohdazm007@gmail.com>
2021-08-26 16:28:20 +0530

    updated js

--- Exponential.js
+++ Exponential.js
@@ -1,7 +1,3 @@
 const arr = [2, 2, 2];
-console.log(
-  arr.reduce((acc, num) => {
-    acc ** num;
-  }),
-  [...arr]
-);
+console.log(arr.reduce((acc, num) => acc + num ** 3, 0)); //method 1
+//console.log(arr.reduce((acc, num) => acc * num ** 3, 1)); //method 2
