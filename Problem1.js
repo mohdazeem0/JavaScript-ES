f48c547ce1dec3b13b55aaca422ca8dc9988958c
mohdazeem0 <mohdazm007@gmail.com>
2021-08-26 16:39:02 +0530

    updated

--- Problem1.js
+++ Problem1.js
@@ -1,2 +1,3 @@
 const arr = [null, 2, 3, 4, "cat", { num: 25 }, undefined, 2];
-const arrCalci=()
+const newArr = arr.filter((item) => typeof item === "numberr" && item);
+console.log(newArr.reduce(acc, (num) => acc + b));
